4cddaa54dab08daa144935d4b562c1097d4ba430
Theo <tdgreer1203@gmail.com>
2022-09-13 17:55:23 -0400

    Working Through Category Routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,8 +13,21 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [ 'category_name' ]
+  }).then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'No categories found with this id' });
+      return;
+    }
+    res.json(dbCategoryData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
